a6be252a7099707a4c8574abf4e4f79f1bf251f9
uo277578 <uo277578@uniovi.es>
2025-06-01 18:58:52 +0200

    Auto-generated commit

---
+++ server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-UE363GED.mjs";import"./chunk-OLW55HAN.mjs";import"./chunk-ZVTFXWI3.mjs";import"./chunk-6TAFF536.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
